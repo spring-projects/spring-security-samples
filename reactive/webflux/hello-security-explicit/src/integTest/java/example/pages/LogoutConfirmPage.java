f2dd39c4bbfa8f698611bbd7a1c3a285fc72db51
Rob Winch <rwinch@users.noreply.github.com>
2020-07-10 15:23:50 -0500

    Add :reactive:webflux:hello-security-explicit

---
+++ reactive/webflux/hello-security-explicit/src/integTest/java/example/pages/LogoutConfirmPage.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * The log out confirmation page.
+ *
+ * @author Rob Winch
+ */
+public class LogoutConfirmPage {
+
+	private final WebDriver webDriver;
+
+	private final LogoutForm logoutForm;
+
+	public LogoutConfirmPage(WebDriver webDriver) {
+		this.webDriver = webDriver;
+		this.logoutForm = PageFactory.initElements(this.webDriver, LogoutForm.class);
+	}
+
+	public LoginPage logout() {
+		return this.logoutForm.logout();
+	}
+
+	public static class LogoutForm {
+
+		private WebDriver webDriver;
+
+		@FindBy(css = "button[type=submit]")
+		private WebElement submit;
+
+		public LogoutForm(WebDriver webDriver) {
+			this.webDriver = webDriver;
+		}
+
+		public LoginPage logout() {
+			this.submit.click();
+			return PageFactory.initElements(this.webDriver, LoginPage.class);
+		}
+
+	}
+
+}
---
+++ reactive/webflux/hello-security-explicit/src/main/java/example/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * Minimal explicit Security configuration.
+ *
+ * @author Rob Winch
+ * @since 5.0
+ */
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+	@Bean
+	SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
+		// @formatter:off
+		http
+			.authorizeExchange((exchanges) -> exchanges
+				.anyExchange().authenticated()
+			)
+			.httpBasic(withDefaults());
+		// @formatter:on
+		return http.build();
+	}
+
+	@Bean
+	MapReactiveUserDetailsService userDetailsService() {
+		// @formatter:off
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.roles("USER")
+			.build();
+		// @formatter:on
+		return new MapReactiveUserDetailsService(user);
+	}
+
+}
