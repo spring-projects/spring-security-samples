4eb69798d5610d2ba10845983f86ccfd9bdb962c
Rob Winch <rwinch@users.noreply.github.com>
2020-07-27 13:49:29 -0500

    Add ":reactive:webflux:authentication:username-password:form"

---
+++ reactive/webflux/authentication/username-password/form/src/main/java/sample/WebfluxFormSecurityConfiguration.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * WebFlux configuration for custom log in page.
+ *
+ * @author Rob Winch
+ * @since 5.0
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class WebfluxFormSecurityConfiguration {
+
+	@Bean
+	public MapReactiveUserDetailsService userDetailsService() {
+		// @formatter:off
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.roles("USER")
+			.build();
+		// @formatter:on
+		return new MapReactiveUserDetailsService(user);
+	}
+
+	@Bean
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		// @formatter:off
+		http
+			.authorizeExchange((exchanges) -> exchanges
+				.pathMatchers("/login").permitAll()
+				.anyExchange().authenticated()
+			)
+			.httpBasic(withDefaults())
+			.formLogin((form) -> form
+				.loginPage("/login")
+			);
+		// @formatter:on
+		return http.build();
+	}
+
+}
