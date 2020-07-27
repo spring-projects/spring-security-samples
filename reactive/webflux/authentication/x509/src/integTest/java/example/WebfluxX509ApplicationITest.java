bde455843cac1070fa6c913d7fd427afbba8b792
Rob Winch <rwinch@users.noreply.github.com>
2020-07-27 15:34:47 -0500

    ":reactive:webflux:authentication:x509"

---
+++ reactive/webflux/authentication/x509/src/integTest/java/example/WebfluxX509ApplicationITest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.SslContextBuilder;
+import org.junit.jupiter.api.Test;
+import reactor.netty.http.client.HttpClient;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WebfluxX509ApplicationITest {
+
+	@LocalServerPort
+	int port;
+
+	@Test
+	void shouldExtractAuthenticationFromCertificate() throws Exception {
+		WebTestClient webTestClient = createWebTestClientWithClientCertificate();
+		// @formatter:off
+		webTestClient.get()
+			.uri("/me")
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.consumeWith((result) -> {
+				String responseBody = new String(result.getResponseBody());
+				assertThat(responseBody).contains("Hello, client");
+			});
+		// @formatter:on
+	}
+
+	private WebTestClient createWebTestClientWithClientCertificate() throws KeyStoreException, IOException,
+			NoSuchAlgorithmException, CertificateException, UnrecoverableEntryException {
+		ClassPathResource serverKeystore = new ClassPathResource("/certs/server.p12");
+
+		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+		keyStore.load(serverKeystore.getInputStream(), "password".toCharArray());
+
+		X509Certificate devCA = (X509Certificate) keyStore.getCertificate("DevCA");
+
+		X509Certificate clientCrt = (X509Certificate) keyStore.getCertificate("client");
+		KeyStore.Entry keyStoreEntry = keyStore.getEntry("client",
+				new KeyStore.PasswordProtection("password".toCharArray()));
+		PrivateKey clientKey = ((KeyStore.PrivateKeyEntry) keyStoreEntry).getPrivateKey();
+
+		// @formatter:off
+		SslContextBuilder sslContextBuilder = SslContextBuilder
+			.forClient().clientAuth(ClientAuth.REQUIRE)
+			.trustManager(devCA)
+			.keyManager(clientKey, clientCrt);
+		// @formatter:on
+
+		HttpClient httpClient = HttpClient.create()
+				.secure((sslContextSpec) -> sslContextSpec.sslContext(sslContextBuilder));
+		ClientHttpConnector httpConnector = new ReactorClientHttpConnector(httpClient);
+
+		// @formatter:off
+		return WebTestClient.bindToServer(httpConnector)
+			.baseUrl("https://localhost:" + this.port)
+			.build();
+		// @formatter:on
+	}
+
+}
---
+++ reactive/webflux/authentication/x509/src/main/java/example/WebfluxX509Application.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * Webflux X509 application.
+ *
+ * @author Alexey Nesterov
+ * @since 5.2
+ */
+@SpringBootApplication
+public class WebfluxX509Application {
+
+	@Bean
+	public ReactiveUserDetailsService reactiveUserDetailsService() {
+		// @formatter:off
+		UserDetails client = User.withUsername("client")
+			.password("")
+			.roles("USER")
+			.build();
+		// @formatter:on
+		return new MapReactiveUserDetailsService(client);
+	}
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		// @formatter:off
+		http
+			.x509(withDefaults())
+			.authorizeExchange((exchanges) -> exchanges
+				.anyExchange().authenticated()
+			);
+		// @formatter:on
+
+		return http.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxX509Application.class);
+	}
+
+}
