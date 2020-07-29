95a4c7e3624cc7cf645aad36c09962b9c72363eb
Rob Winch <rwinch@users.noreply.github.com>
2020-07-29 17:27:07 -0500

    ":reactive:webflux:kotlin:hello-security"

---
+++ reactive/webflux/kotlin/hello-security/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("jvm") version "1.3.71"
+    kotlin("plugin.spring") version "1.3.71"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.spring.io/libs-snapshot")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- sync-gradle.sh
+++ sync-gradle.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find -mindepth 2 -name build.gradle | xargs dirname | xargs -I{} sh -c "cp './gradlew' {}; cp ./gradlew.bat {} ; cp -r gradle/wrapper {}/gradle/"
+find -mindepth 2 -name "build.gradle*" | xargs dirname | xargs -I{} sh -c "cp './gradlew' {}; cp ./gradlew.bat {} ; cp -r gradle/wrapper {}/gradle/"
