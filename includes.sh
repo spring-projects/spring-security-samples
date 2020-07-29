7c3535b65afe5f144deaee46984e1b3030a2918c
Rob Winch <rwinch@users.noreply.github.com>
2020-07-29 17:13:12 -0500

    includes sorts

--- includes.sh
+++ includes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find -name build.gradle | sed -E 's#\.?/#:#g' | sed 's#:build.gradle$##' | xargs -I{} echo "include \"{}\""
+find -name "build.gradle*" | sed -E 's#\.?/#:#g' | sed 's#:build.gradle.*$##' | xargs -I{} echo "include \"{}\"" | sort
