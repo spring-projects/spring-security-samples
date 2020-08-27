6a0f1ab4f13a0f1f2009f83c7f621c8c7b46bfda
Rob Winch <rwinch@users.noreply.github.com>
2020-08-27 15:21:53 -0500

    sync-boot-version.sh

---
+++ sync-boot-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export BOOT_VERSION=$1
+
+if [[ "$BOOT_VERSION" == "" ]]; then
+  echo "Missing boot-version. Usage: sync-boot-version.sh <boot-version>"
+  exit 1
+fi
+find -mindepth 2 -name build.gradle | xargs sed -i -E "s/(id 'org.springframework.boot' version ').*?'/\1$BOOT_VERSION'/"
