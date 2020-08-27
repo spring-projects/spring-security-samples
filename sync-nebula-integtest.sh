fdd8b2bf6bd426effd6dd1ab840210ee62d07181
Rob Winch <rwinch@users.noreply.github.com>
2020-08-27 15:31:08 -0500

    sync-nebula-integtest.sh

---
+++ sync-nebula-integtest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export VERSION=$1
+
+if [[ "VERSION" == "" ]]; then
+  echo "Missing version. Usage: sync-nebula-integtest.sh <version>"
+  exit 1
+fi
+find -mindepth 2 -name build.gradle | xargs sed -i -E "s/(id 'nebula.integtest' version ').*?'/\1$VERSION'/"
