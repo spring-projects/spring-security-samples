96c752684370c7f6611e369ba492c1455c657188
Rob Winch <rwinch@users.noreply.github.com>
2020-08-27 15:25:34 -0500

    sync-dependency-management.sh

---
+++ sync-dependency-management.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export VERSION=$1
+
+if [[ "VERSION" == "" ]]; then
+  echo "Missing version. Usage: sync-dependency-management.sh <version>"
+  exit 1
+fi
+find -mindepth 2 -name build.gradle | xargs sed -i -E "s/(id 'io.spring.dependency-management' version ').*?'/\1$VERSION'/"
