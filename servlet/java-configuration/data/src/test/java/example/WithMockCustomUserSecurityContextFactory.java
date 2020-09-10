75770dd6c57a1045b33cba4a2c58c91e38116a54
Rob Winch <rwinch@users.noreply.github.com>
2020-09-10 14:56:33 -0500

    Add ":servlet:java-configuration:data"

---
+++ servlet/java-configuration/data/src/test/java/example/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+
+	@Override
+	public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
+		SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+		User principal = new User();
+		principal.setId(customUser.id());
+		principal.setEmail(customUser.email());
+		Authentication auth = new UsernamePasswordAuthenticationToken(principal, "password",
+				AuthorityUtils.createAuthorityList("ROLE_USER"));
+		context.setAuthentication(auth);
+		return context;
+	}
+
+}
