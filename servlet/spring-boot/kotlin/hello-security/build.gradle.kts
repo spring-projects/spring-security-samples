43dbe19507fd29becec976e89ad35cc0b6bf3036
Josh Cummings <josh.cummings@gmail.com>
2021-05-27 10:04:32 -0600

    Update Kotlin Samples
    Closes gh-19

--- servlet/spring-boot/kotlin/hello-security/build.gradle.kts
+++ servlet/spring-boot/kotlin/hello-security/build.gradle.kts
@@ -1,16 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("org.springframework.boot") version "2.5.0"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.3.71"
-    kotlin("plugin.spring") version "1.3.71"
+    kotlin("jvm") version "1.5.0"
+    kotlin("plugin.spring") version "1.5.0"
 }
 
 repositories {
     mavenCentral()
     maven {
-        url = uri("https://repo.spring.io/libs-snapshot")
+        url = uri("https://repo.spring.io/snapshot")
     }
 }
 
