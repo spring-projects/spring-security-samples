fd73ddc121b7cb034c7bfd7bc5b85405c6d67b35
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-10-10 08:00:16 -0300

    Use ignoringRequestMatchers
    Issue gh-96

--- servlet/spring-boot/java/jwt/login/src/main/java/example/RestConfig.java
+++ servlet/spring-boot/java/jwt/login/src/main/java/example/RestConfig.java
@@ -65,7 +65,7 @@ public class RestConfig {
 				.authorizeHttpRequests((authorize) -> authorize
 						.anyRequest().authenticated()
 				)
-				.csrf((csrf) -> csrf.ignoringAntMatchers("/token"))
+				.csrf((csrf) -> csrf.ignoringRequestMatchers("/token"))
 				.httpBasic(Customizer.withDefaults())
 				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
 				.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
