24dc56e6ca3d61c9206df622921bfe0b36af91a2
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-03-31 09:28:45 -0300

    Update AspectJ sample to Java 17

--- servlet/java-configuration/aspectj/src/main/java/sample/aspectj/AspectjSecurityConfig.java
+++ servlet/java-configuration/aspectj/src/main/java/sample/aspectj/AspectjSecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -31,7 +32,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  * @author Rob Winch
  */
 @Configuration
-@EnableGlobalMethodSecurity(mode = AdviceMode.ASPECTJ, securedEnabled = true)
+@EnableMethodSecurity(mode = AdviceMode.ASPECTJ, securedEnabled = true, prePostEnabled = false)
 public class AspectjSecurityConfig {
 
 	@Bean
