33052562bc8eaafcf509e48d5d1b33964d9a691b
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-06-21 10:52:50 -0300

    Fix sync scripts
    Closes gh-134

--- sync-boot-version.sh
+++ sync-boot-version.sh
@@ -6,4 +6,4 @@ if [[ "$BOOT_VERSION" == "" ]]; then
   echo "Missing boot-version. Usage: sync-boot-version.sh <boot-version>"
   exit 1
 fi
-find -mindepth 2 -name build.gradle | xargs sed -i -E "s/(id 'org.springframework.boot' version ').*?'/\1$BOOT_VERSION'/"
+find . -mindepth 2 -name build.gradle | xargs sed -i '' -E "s/(id 'org.springframework.boot' version ')[^']+'/\1$BOOT_VERSION'/"
