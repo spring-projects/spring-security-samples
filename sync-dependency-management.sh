8d8e6d005680d922bce0ad0d746c9724c4d68d44
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-07-20 10:38:53 -0300

    Revert "Fix sync scripts"
    This reverts commit 33052562bc8eaafcf509e48d5d1b33964d9a691b.

--- sync-dependency-management.sh
+++ sync-dependency-management.sh
@@ -6,4 +6,4 @@ if [[ "VERSION" == "" ]]; then
   echo "Missing version. Usage: sync-dependency-management.sh <version>"
   exit 1
 fi
-find . -mindepth 2 -name build.gradle | xargs sed -i '' -E "s/(id 'io.spring.dependency-management' version ')[^']+'/\1$VERSION'/"
+find -mindepth 2 -name build.gradle | xargs sed -i -E "s/(id 'io.spring.dependency-management' version ').*?'/\1$VERSION'/"
