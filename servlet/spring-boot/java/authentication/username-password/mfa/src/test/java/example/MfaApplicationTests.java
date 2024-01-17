d3fba5014e3c3edc9038752969bdb32c0cca5d7b
Marcus Hert Da Coregio <marcusdacoregio@gmail.com>
2024-01-17 14:25:18 -0300

    Fix checkstyle errors
    (cherry picked from commit bd98343b9c624bbd526ce3385e23ac7dfb55a740)

--- servlet/spring-boot/java/authentication/username-password/mfa/src/test/java/example/MfaApplicationTests.java
+++ servlet/spring-boot/java/authentication/username-password/mfa/src/test/java/example/MfaApplicationTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package example;
 
 import com.j256.twofactorauth.TimeBasedOneTimePasswordUtil;
