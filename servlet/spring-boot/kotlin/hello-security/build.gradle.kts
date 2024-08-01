414bb7d241e399362f83b6fcc0c548a9ee67d7cd
Josh Cummings <josh.cummings@gmail.com>
2024-08-01 08:08:17 -0600

    Update Kotlin Samples to Use Dependabot Versions

--- servlet/spring-boot/kotlin/hello-security/build.gradle.kts
+++ servlet/spring-boot/kotlin/hello-security/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.0"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
+    alias(libs.plugins.org.springframework.boot)
+    alias(libs.plugins.io.spring.dependency.management)
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
 }
 
 repositories {
