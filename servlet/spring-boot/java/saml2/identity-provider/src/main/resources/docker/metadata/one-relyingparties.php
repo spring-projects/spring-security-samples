55d1b15b1257d0ac1a5a55f25bdc329c17f3b2e2
Josh Cummings <josh.cummings@gmail.com>
2024-11-07 11:27:44 -0700

    Fix Identity Provider Configurations

--- servlet/spring-boot/java/saml2/identity-provider/src/main/resources/docker/metadata/one-relyingparties.php
+++ servlet/spring-boot/java/saml2/identity-provider/src/main/resources/docker/metadata/one-relyingparties.php
@@ -4,8 +4,8 @@ $port = getenv("PORT");
 // Spring Security SP
 
 $metadata["http://localhost:$port/saml2/metadata"] = array(
-    'AssertionConsumerService' => "https://localhost:$port/login/saml2/sso",
-    'SingleLogoutService' => "https://localhost:$port/logout/saml2/slo",
+    'AssertionConsumerService' => "http://localhost:$port/login/saml2/sso",
+    'SingleLogoutService' => "http://localhost:$port/logout/saml2/slo",
     'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
     'simplesaml.nameidattribute' => 'emailAddress',
     'assertion.encryption' => FALSE,
