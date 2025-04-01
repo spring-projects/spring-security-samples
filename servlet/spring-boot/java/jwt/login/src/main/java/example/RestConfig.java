6cda335d2d0600dfa92a3fe82155aea6e2f04544
XiaotianZha <bad58n81so@126.com>
2025-04-01 09:33:44 +0800

    Update RestConfig.java
    OAuth2ResourceServerConfigurer#jwt() deprecated since 6.1
    
    Signed-off-by: XiaotianZha <bad58n81so@126.com>

--- servlet/spring-boot/java/jwt/login/src/main/java/example/RestConfig.java
+++ servlet/spring-boot/java/jwt/login/src/main/java/example/RestConfig.java
@@ -67,7 +67,7 @@ public class RestConfig {
 				)
 				.csrf((csrf) -> csrf.ignoringRequestMatchers("/token"))
 				.httpBasic(Customizer.withDefaults())
-				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
 				.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.exceptionHandling((exceptions) -> exceptions
 						.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
