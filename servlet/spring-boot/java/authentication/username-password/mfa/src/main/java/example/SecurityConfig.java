bdcf2780c600e128a1e8948acac633f7cf6db884
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-07-18 14:28:58 -0600

    Update :servlet:spring-boot:java:authentication:username-password to 7

--- servlet/spring-boot/java/authentication/username-password/mfa/src/main/java/example/SecurityConfig.java
+++ servlet/spring-boot/java/authentication/username-password/mfa/src/main/java/example/SecurityConfig.java
@@ -23,7 +23,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
