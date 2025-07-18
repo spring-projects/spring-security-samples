84f51b5f4bac62993e5639bae1955766d766aedf
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-07-18 15:45:09 -0600

    Update :servlet:java-configuration:authentication:preauth to 7

--- servlet/java-configuration/authentication/preauth/src/main/java/example/SecurityConfiguration.java
+++ servlet/java-configuration/authentication/preauth/src/main/java/example/SecurityConfiguration.java
@@ -18,7 +18,6 @@ package example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -26,30 +25,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, PathPatternRequestMatcher.Builder mvc)
+			throws Exception {
 		// @formatter:off
 		http
 				.authorizeHttpRequests((authorize) -> authorize
-						.requestMatchers(mvc.pattern("/login"), mvc.pattern("/resources/**")).permitAll()
+						.requestMatchers(mvc.matcher("/login"), mvc.matcher("/resources/**")).permitAll()
 						.anyRequest().authenticated()
 				)
 				.jee((jee) -> jee.mappableRoles("USER", "ADMIN"));
 		// @formatter:on
 		return http.build();
-	}
-
-	@Scope("prototype")
-	@Bean
-	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-		return new MvcRequestMatcher.Builder(introspector);
 	}
 
 	// @formatter:off
