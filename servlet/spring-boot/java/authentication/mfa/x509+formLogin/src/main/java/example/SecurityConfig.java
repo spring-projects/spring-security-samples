f2640279c4fafd708cb562f4ce463421f9bcb6ca
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-09-24 17:27:08 -0600

    Add X.509 + FormLogin MFA Sample

---
+++ servlet/spring-boot/java/authentication/mfa/x509+formLogin/src/main/java/example/SecurityConfig.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authorization.EnableGlobalMultiFactorAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.core.GrantedAuthorities.FACTOR_PASSWORD_AUTHORITY;
+import static org.springframework.security.core.GrantedAuthorities.FACTOR_X509_AUTHORITY;
+
+@Configuration(proxyBeanMethods = false)
+@EnableGlobalMultiFactorAuthentication(authorities = { FACTOR_X509_AUTHORITY, FACTOR_PASSWORD_AUTHORITY })
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain web(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+			.x509(Customizer.withDefaults())
+			.formLogin(Customizer.withDefaults());
+		// @formatter:on
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService users() {
+		return new InMemoryUserDetailsManager(
+			User.withDefaultPasswordEncoder()
+				.username("josh")
+				.password("password")
+				.authorities("app")
+				.build()
+		);
+	}
+
+}
---
+++ servlet/spring-boot/java/authentication/mfa/x509+formLogin/src/test/java/example/MfaApplicationTests.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.core.GrantedAuthorities.FACTOR_PASSWORD_AUTHORITY;
+import static org.springframework.security.core.GrantedAuthorities.FACTOR_X509_AUTHORITY;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Rob Winch
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class MfaApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void indexWhenUnauthenticatedThenRedirectsToLogin() throws Exception {
+		this.mvc.perform(get("/"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("http://localhost/login"));
+	}
+
+	@Test
+	@WithMockUser
+	void indexWhenAuthenticatedButNoFactorsThenForbidden() throws Exception {
+		this.mvc.perform(get("/"))
+			.andExpect(status().isForbidden());
+	}
+
+	@Test
+	@WithMockUser(authorities = FACTOR_X509_AUTHORITY)
+	void indexWhenAuthenticatedWithX509ThenRedirectsToLogin() throws Exception {
+		this.mvc.perform(get("/"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("http://localhost/login?factor=password"));
+	}
+
+	@Test
+	@WithMockUser(authorities = FACTOR_PASSWORD_AUTHORITY)
+	void indexWhenAuthenticatedWithPasswordThenForbidden() throws Exception {
+		this.mvc.perform(get("/"))
+			.andExpect(status().isForbidden());
+	}
+
+
+}
