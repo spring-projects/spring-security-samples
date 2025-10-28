1388a20b63b1e52cefc420d5f405abd3faf2fcb3
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-10-28 10:43:00 -0500

    Fix LocalServerPort

--- servlet/spring-boot/java/saml2/saml-extension-urls/src/integTest/java/example/SamlExtensionUrlsApplicationITests.java
+++ servlet/spring-boot/java/saml2/saml-extension-urls/src/integTest/java/example/SamlExtensionUrlsApplicationITests.java
@@ -31,9 +31,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.test.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
